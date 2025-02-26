659e0f6e497d290dcee443f60ba2c0c9c94c163b
Pedro MP <126501531+PMP2006@users.noreply.github.com>
2025-02-26 19:50:33 +0000

    botao

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebaseConfig: {
-    apiKey: "AIzaSyCGdwCWx_8o2hYEmLBhK6xQzCMp5h12gzk",
-    authDomain: "aula-3e82a.firebaseapp.com",
-    projectId: "aula-3e82a",
-    storageBucket: "aula-3e82a.firebasestorage.app",
-    messagingSenderId: "258284807710",
-    appId: "1:258284807710:web:72b06b67cea8d15cd6b00f",
-    measurementId: "G-X8L8GS7897"
+    apiKey: "AIzaSyBxi4TJ_0X5DDNAxyNN2l5y0vD5A6oI_DQ",
+    authDomain: "aula1-8dba1.firebaseapp.com",
+    projectId: "aula1-8dba1",
+    storageBucket: "aula1-8dba1.firebasestorage.app",
+    messagingSenderId: "46312787113",
+    appId: "1:46312787113:web:e3a471feb42427c1f9edb8",
+    measurementId: "G-D1KCDV1VCP"
   },
   production: true
 };
